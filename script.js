4543ae7c00b7cae850fd8fea16432181897c46ad
Noshi96 <2spawel2@gmail.com>
2021-11-26 20:30:08 +0100

    Zadanie-1 1. Total price order method using reduce now, 2. Improved adding and removing items to basket code, 3. Changed all cart names to basket, 4. Ingredients are now capitalized by CSS

--- script.js
+++ script.js
@@ -4,20 +4,21 @@
 let basket = [];
 
 document.addEventListener('DOMContentLoaded', function () {
-	getAllPizzas();
+    getAllPizzas();
 });
 
 /**
  * Fetch Pizzas data from given JSON.
  */
 function getAllPizzas() {
-	fetch('https://raw.githubusercontent.com/alexsimkovich/patronage/main/api/data.json')
-		.then(response => response.json())
-		.then(data => {
-			data.forEach(pizza => {
-				createPizzaItem(pizza);
-			});
-		});
+
+    fetch('https://raw.githubusercontent.com/alexsimkovich/patronage/main/api/data.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(pizza => {
+                createPizzaItem(pizza);
+            });
+        });
 }
 
 /**
@@ -28,19 +29,18 @@ function getAllPizzas() {
  */
 function createPizzaItem(pizza) {
 
-	const id = pizza.id;
-	const title = pizza.title;
-	const price = parseFloat(pizza.price).toFixed(2);
-	const ingredients = pizza.ingredients;
-	const imageUrl = pizza.image;
+    const id = pizza.id;
+    const title = pizza.title;
+    const price = parseFloat(pizza.price).toFixed(2);
+    const ingredients = pizza.ingredients;
+    const imageUrl = pizza.image;
 
-	const ingredientsString = ingredients
-		.map(ingredient => capitalize(ingredient))
-		.join(', ');
+    const ingredientsString = ingredients
+        .join(', ');
 
-	const menuItem = document.createElement('div');
-	menuItem.className = "menu-item";
-	menuItem.innerHTML = `
+    const menuItem = document.createElement('div');
+    menuItem.className = "menu-item";
+    menuItem.innerHTML = `
 	 <img class="menu-item-image" src="${imageUrl}" alt="${title}">
 	 <div class="menu-item-text">
 		 <h3 class="menu-item-heading">
@@ -55,77 +55,89 @@ function createPizzaItem(pizza) {
 	 </div>
    `;
 
-	menuItem.querySelector('.menu-item-button').addEventListener('click', () => {
-		createCartItem(id, title, price);
-	});
-	document.querySelector('#menu-group').append(menuItem);
+    menuItem.querySelector('.menu-item-button').addEventListener('click', () => {
+        addToBasket(id, price, title);
+    });
+
+    document.querySelector('#menu-group').append(menuItem);
 }
 
 /**
- * Capitalized first letter in String.
- * @param {*} s Given String.
- * @returns String with first capitalized letter.
- */
-const capitalize = (s) => {
-	if (typeof s !== 'string') return ''
-	return s.charAt(0).toUpperCase() + s.slice(1)
-}
-
-/**
- * If pizza is not in the cart, creates new item in basket with values such as [Pizza name{title}, Pizza price{price}, Pizza id{id}, Counted same pizza type in the cart{count}].
- * If pizza exist update Total price and update {count}.
- * onClickListener on delete button.
- * @param {*} title Name of pizza.
- * @param {*} price Price of pizza.
+ * Add item to basket.
+ * Check if pizza exist in basket.
+ * If yes, increase count +1.
+ * If no, add pizza to basket list.
  * @param {*} id Identifier of specific pizza.
+ * @param {*} price Price of specific pizza.
+ * @param {*} title Name of specific pizza.
  */
-function createCartItem(id, title, price) {
+function addToBasket(id, price, title) {
 
-	const pizzaInCart = basket.find(item => item.id === id)
-	if (pizzaInCart) {
-		const pizzaIdInList = basket.findIndex((pizza => pizza.id === id));
-		basket[pizzaIdInList].count++;
-		document.querySelector(`#pizza_id_${id}`).querySelector('.cart-item-count').innerHTML = basket[pizzaIdInList].count;
-		document.querySelector('.total-price-order').innerHTML = calculateUpdatedTotalPriceOrder(basket);
-	} else {
+    const pizzaIdInList = basket.findIndex((pizza => pizza.id === id));
 
-		basket.push({
-			id: id,
-			price: price,
-			title: title,
-			count: 1
-		});
+    if (pizzaIdInList !== -1) {
+        basket[pizzaIdInList].count++;
+    } else {
+        basket.push({
+            id: id,
+            price: price,
+            title: title,
+            count: 1
+        });
+    }
+    renderBasket();
+}
 
-		const cartItem = document.createElement('div');
-		cartItem.className = "cart-item";
-		cartItem.id = `pizza_id_${id}`;
-		cartItem.innerHTML = `
-			 <div class="cart-item-name">
-				 <h3 class="cart-item-heading">
-					 ${title}
+/**
+ * Remove pizza from basket
+ * If there is only one pizza, remove it.
+ * If there is more then 1 pizza decrease count -1;
+ * @param {*} id Identifier of specific pizza in basket.
+ */
+function removeFromBasket(id) {
+
+    const pizzaIdInList = basket.findIndex((pizza => pizza.id === id));
+    
+    if (basket[pizzaIdInList].count === 1) {
+        basket = basket.filter(pizza => pizza.id !== id);
+    } else {
+        basket[pizzaIdInList].count--;
+    }
+    renderBasket();
+}
+
+/**
+ * Render basket with pizzas
+ * Add onClickListener on button to call remove function.
+ */
+function renderBasket() {
+
+    document.querySelector('.basket-group').innerHTML = "";
+
+    basket.forEach(pizzaInBasket => {
+        const basketItem = document.createElement('div');
+        basketItem.className = "basket-item";
+        basketItem.id = `pizza_id_${pizzaInBasket.id}`;
+        basketItem.innerHTML = `
+			 <div class="basket-item-name">
+				 <h3 class="basket-item-heading">
+					 ${pizzaInBasket.title}
 				 </h3>
 			 </div>
-			 <div class="cart-item-count">1</div>
-			 <div class="cart-item-button-div">
-				 <button class="cart-item-button">Usuń</button>
+			 <div class="basket-item-count">${pizzaInBasket.count}</div>
+			 <div class="basket-item-button-div">
+				 <button class="basket-item-button">Usuń</button>
 			 </div>
-			 <div class="cart-item-price">${price}zł</div>
+			 <div class="basket-item-price">${pizzaInBasket.price}zł</div>
 		 `;
 
-		cartItem.querySelector('.cart-item-button').addEventListener('click', () => {
-			const pizzaIdInList = basket.findIndex((pizza => pizza.id === id));
-			if (basket[pizzaIdInList].count === 1) {
-				basket = basket.filter(pizza => pizza.id !== id);
-				cartItem.parentNode.removeChild(cartItem);
-			} else {
-				basket[pizzaIdInList].count--;
-				document.querySelector(`#pizza_id_${id}`).querySelector('.cart-item-count').innerHTML = basket[pizzaIdInList].count;
-			}
-			document.querySelector('.total-price-order').innerHTML = calculateUpdatedTotalPriceOrder(basket);
-		});
-		document.querySelector('.total-price-order').innerHTML = calculateUpdatedTotalPriceOrder(basket);
-		document.querySelector('.cart-group').append(cartItem);
-	}
+        basketItem.querySelector('.basket-item-button').addEventListener('click', () => {
+            removeFromBasket(pizzaInBasket.id);
+        });
+
+        document.querySelector('.basket-group').append(basketItem);
+    });
+    document.querySelector('.total-price-order').innerHTML = calculateUpdatedTotalPriceOrder(basket);
 }
 
 /** 
@@ -134,13 +146,11 @@ function createCartItem(id, title, price) {
  * @returns When there are no pizzas in basket, return "Głodny? Zamów naszą pizzę" else total price of orders.
  */
 function calculateUpdatedTotalPriceOrder(basket) {
-	let totalPriceOrder = 0.0;
-	basket
-		.map(pizza => {
-			totalPriceOrder += pizza.price * pizza.count;
-		});
-	if (totalPriceOrder === 0.0) {
-		return "Głodny? Zamów naszą pizzę";
-	}
-	return (totalPriceOrder.toFixed(2) + " zł");
+
+    let totalPriceOrder = basket.reduce((a, b) => a + b.price * b.count, 0);
+
+    if (totalPriceOrder === 0.0) {
+        return "Głodny? Zamów naszą pizzę";
+    }
+    return (totalPriceOrder.toFixed(2) + " zł");
 }
